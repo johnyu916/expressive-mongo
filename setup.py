a1114612facbcd785c095e061bd0ea59c4acb4ec
John Yu <johnyu916@hotmail.com>
2020-01-31 13:54:22 -0500

    Update version

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
 from distutils.core import setup
 setup(
-  name = 'expressive-mongo',         # How you named your package folder (MyLib)
-  packages = ['expressive_mongo'],   # Chose the same as "name"
-  version = '0.0.1',      # Start with a small number and increase it with every change you make
-  license= 'Apache License, Version 2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Easy MongoDB queries with Python expressions',   # Give a short description about your library
-  author = 'John Yu',                   # Type in your name
-  author_email = 'johnyu916@hotmail.com',      # Type in your E-Mail
-  url = 'https://github.com/johnyu916/expressive-mongo',   # Provide either the link to your github or to your website
+  name = 'expressive-mongo',
+  packages = ['expressive_mongo'],
+  version = '0.0.2',
+  license= 'Apache License, Version 2.0',
+  description = 'Easy MongoDB queries with Python expressions',
+  author = 'John Yu',
+  author_email = 'johnyu916@hotmail.com',
+  url = 'https://github.com/johnyu916/expressive-mongo',
   download_url = 'https://github.com/johnyu916/expressive-mongo/archive/0.0.1.tar.gz',
-  keywords = ['MongoDB', 'Database', 'Query'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'pymongo',
-      ],
+  keywords = ['MongoDB', 'Database', 'Query'],
+  install_requires=['pymongo',],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
     'Intended Audience :: Developers',
     'Topic :: Database',
-    'License :: OSI Approved :: Apache Software License',   # Again, pick a license
+    'License :: OSI Approved :: Apache Software License',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
